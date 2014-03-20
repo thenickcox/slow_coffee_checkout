d163dd5b9555fc44eb8836b9d416c01d829d6bf3
Nick Cox <nick@nvigatingcancer.com>
2014-03-20 15:36:34 -0700

    Upgrade Selenium and change test

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -13,21 +13,16 @@ describe 'checkout', type: :feature, js: true do
     page.should have_content 'Amount: $20.00'
     page.find('#button-two').click_button 'Pay with Card'
     Capybara.within_frame 'stripe_checkout_app' do
-      sleep 3
-      find('.overlay') do
-        within 'div.panel' do
-          fill_in 'Email', with: 'iamnicholascox@gmail.com'
-          fill_in 'Name',  with: 'Nick Cox'
-          fill_in 'Street', with: '7352 11th Ave NW'
-          fill_in 'ZIP', with: '98117'
-          fill_in 'City', with: 'Seattle'
-          click_button 'Payment Info'
-          fill_in 'Card number', with: '4242424242424242'
-          fill_in 'MM/YY', with: '02/22'
-          fill_in 'CVC', with: '222'
-          click_button 'Pay'
-        end
-      end
+      fill_in 'Email', with: 'iamnicholascox@gmail.com'
+      fill_in 'Name',  with: 'Nick Cox'
+      fill_in 'Street', with: '7352 11th Ave NW'
+      fill_in 'ZIP', with: '98117'
+      fill_in 'City', with: 'Seattle'
+      click_button 'Payment Info'
+      fill_in 'Card number', with: '4242424242424242'
+      fill_in 'MM/YY', with: '02/22'
+      fill_in 'CVC', with: '222'
+      click_button 'Pay'
     end
     page.should have_content 'Thanks'
   end
